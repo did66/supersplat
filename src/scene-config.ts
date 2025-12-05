84b23298e0c2f2ee2c0afde00eb71a7c0b8c8816
刘志鹏 <329225920@qq.com>
2025-12-05 16:41:43 +0800

    feat:默认背景颜色修改

--- src/scene-config.ts
+++ src/scene-config.ts
@@ -1,6 +1,6 @@
 type Color = { r: number, g: number, b: number, a: number };
 
-const DEFAULT_BG_CLR: Color = { r: 0, g: 0, b: 0, a: 1 };
+const DEFAULT_BG_CLR: Color = { r: 248 / 255, g: 243 / 255, b: 238 / 255, a: 1 };
 const DEFAULT_SELECTED_CLR: Color = { r: 1, g: 1, b: 0, a: 1 };
 const DEFAULT_UNSELECTED_CLR: Color = { r: 0, g: 0, b: 1, a: 0.5 };
 const DEFAULT_LOCKED_CLR: Color = { r: 0, g: 0, b: 0, a: 0.05 };
