ac91b8549c7b9d00dd4fbc9d17dc1106023162ff
djd <win944@tutamail.com>
2025-12-24 10:28:14 +0800

    feat:版本修改

--- src/ui/editor.ts
+++ src/ui/editor.ts
@@ -141,7 +141,7 @@ class EditorUI {
 		}
 		// canvasContainer.append(rightToolbar);
 		// canvasContainer.append(modeToggle);
-		canvasContainer.append(menu);
+		// canvasContainer.append(menu);
 
 		// view axes container
 		const viewCube = new ViewCube(events);
